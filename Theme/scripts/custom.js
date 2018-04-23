86c65e6623819779c60623dfc80df513450aab75
Asaf Slook <asafsloook@gmail.com>
2018-04-23 15:51:42 +0300

    map searchbox

--- Theme/scripts/custom.js
+++ Theme/scripts/custom.js
@@ -77,6 +77,7 @@ function onSuccess(position) {
 
 function onError(error) {
     alert("Please activate Location services and refresh the page");
+    getNewWiki();
 }
 
 function getMyPosition() {
@@ -933,6 +934,9 @@ $(document).ready(function () {
             });
 
 
+            initAutocomplete();
+
+
             $('#myLocationBTN').on('click', function () {
                 closeAllInfoWindows();
                 getMyPosition();
@@ -944,16 +948,59 @@ $(document).ready(function () {
                 getNewWiki();
             });
 
-            getMyPosition();
+            if (typeof fromAroundArticle === 'undefined') {
+                getMyPosition();
+            }
+            else {
+                getNewWiki();
+            }
+            
         }
 
         if (window.location.href.toString().indexOf('articlearound.html') != -1) {
 
 
             getArticleById(localStorage.lastPageid);
+            fromAroundArticle = true;
 
         }
 
+        function initAutocomplete() {
+            
+
+            // Create the search box and link it to the UI element.
+            var input = document.getElementById('pac-input');
+            var searchBox = new google.maps.places.SearchBox(input);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+            // Bias the SearchBox results towards current map's viewport.
+            map.addListener('bounds_changed', function () {
+                searchBox.setBounds(map.getBounds());
+            });
+            
+            // Listen for the event fired when the user selects a prediction and jump to it
+            searchBox.addListener('places_changed', function () {
+                var places = searchBox.getPlaces();
+
+                if (places.length == 0) {
+                    return;
+                }
+                
+                // For each place, get the icon, name and location.
+                var bounds = new google.maps.LatLngBounds();
+                places.forEach(function (place) {
+                    
+                    if (place.geometry.viewport) {
+                        // Only geocodes have viewport.
+                        bounds.union(place.geometry.viewport);
+                    } else {
+                        bounds.extend(place.geometry.location);
+                    }
+                });
+                map.fitBounds(bounds);
+                getNewWiki();
+            });
+        }
 
 
         function checkUser2(request) {
