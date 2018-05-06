c288444dd606189b453481dd04bcf9e593d19aba
Asaf Slook <asafsloook@gmail.com>
2018-05-06 10:55:24 +0300

    oc

--- App_Code/Article.cs
+++ App_Code/Article.cs
@@ -95,7 +95,7 @@ public class Article
 
         string ResponseText;
         HttpWebRequest myRequest2 =
-        (HttpWebRequest)WebRequest.Create("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&format=json&titles=" + "Charles Fourier");
+        (HttpWebRequest)WebRequest.Create("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&format=json&titles=" + articleTitle);
         using (HttpWebResponse response = (HttpWebResponse)myRequest2.GetResponse())
         {
             using (StreamReader reader = new StreamReader(response.GetResponseStream()))
